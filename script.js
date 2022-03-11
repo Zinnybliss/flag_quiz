2d48783f5521b25dee44831f5ce57a9b7cc0d6bf
Zinnybliss <50361623+Zinnybliss@users.noreply.github.com>
2022-03-11 15:41:38 +0100

    body

---
+++ script.js
@@ -0,0 +1,54 @@
+ const btnElement = document.getElementById("btn");
+ const imgElement = document.getElementById("imgbig");
+ const answerElement = document.getElementById("answer");
+ 
+
+
+btnElement.addEventListener("click", function(e){
+   allCode()
+})
+const newButton = document.querySelectorAll("input")
+
+function allCode(){
+let correctAnswer = ""
+let aNewOption = []
+for (let i = 0; i < newButton.length; i += 1)
+{    
+   const randomButton = Math.floor(Math.random()*countryWithImages.length)
+const allButton = countryWithImages[randomButton].name
+const butButton = countryWithImages[randomButton].file_url
+genButton = {butButton, allButton}
+aNewOption.push(genButton)
+// console.log(aNewOption)
+
+
+
+
+   newButton[i].addEventListener("click", function(e){
+      let allTrue = e.currentTarget.value
+if (allTrue === correctAnswer){
+   console.log(true)
+   answerElement.textContent = "Yaay! you are right"
+   
+}
+else{
+   console.log (false)
+   answerElement.textContent = "Ouch! you are wrong"
+}
+
+   })
+newButton[i].value = allButton
+console.log(newButton[i].nextElementSibling)
+newButton[i].nextElementSibling.textContent = allButton
+}
+
+aNewCountry = Math.floor(Math.random()*aNewOption.length)
+
+let someCountry = aNewOption[aNewCountry]
+
+correctAnswer = someCountry.allButton
+console.log(correctAnswer)
+imgElement.src=`https:${someCountry.butButton}`
+}
+
+allCode()
